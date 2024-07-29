e41c90acef7b785abef0ddde57f8e650edf09a85
Pierre Chalamet <pierre@chalamet.net>
2024-07-29 13:33:03 +0200

    add fun

--- MiniML.Parser/AbstractSyntaxTree.fs
+++ MiniML.Parser/AbstractSyntaxTree.fs
@@ -1,7 +1,7 @@
 module MiniML.AbstractSyntaxTree
  
 [<RequireQualifiedAccess>]
-type Function =
+type Operator =
     | Plus
     | Minus
     | Times
@@ -17,26 +17,27 @@ type Function =
     | Not
 
 [<RequireQualifiedAccess>]
-type Expression = 
-    | Number of decimal
-    | Symbol of string
-    | Function of Function * Expression * Expression
-    | FunctionCall of string * Expression list
-    | PartialFunctionCall of string * (Expression list)
-
-[<RequireQualifiedAccess>]
 type Binding =
     | Symbol of string
 
 [<RequireQualifiedAccess>]
-type Statement =
+type Expression = 
+    | Number of decimal
+    | Symbol of string
+    | Operator of Operator * Expression * Expression
+    | Invoke of string * Expression list
+    | Function of Binding * Statements
+and
+    [<RequireQualifiedAccess>] 
+    Statement =
     | Return of Expression
     | Let of Binding * Statements
     | IfThenElse of Expression * Statements * Statements
     | Match of Expression * (Expression * Statement list) list
     | While of Expression * Statement list
     | For of Binding * Expression * Statement list
-and Statements = Statement list
+and
+    Statements = Statement list
 
 [<RequireQualifiedAccess>]
 type File =
